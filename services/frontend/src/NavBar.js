5374062f6572b3a22a5a3cb8307ffc14dab09684
Jesse Shaw <JesseShawCodes@Gmail.com>
2024-05-01 14:10:59 -0400

    Minor Navbar updates

--- services/frontend/src/NavBar.js
+++ services/frontend/src/NavBar.js
@@ -14,11 +14,19 @@ const NavBar = () => {
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <Link to="/about" className='nav-link active'>About</Link>
-                    </li>
-                </ul>
+                    
+                        {
+                            user ?
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link to="/about" className='nav-link active'>About</Link> 
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/export" className='nav-link active'>Export</Link> 
+                            </li>
+                            </ul>
+                            : ""
+                        }
                 <ul className='navbar-nav'>
                     {
                         user ? <li className='nav-item me-auto mb-2 mb-lg-0'>{user.email}</li> : ""
