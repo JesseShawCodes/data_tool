b4db39edd7d1af2962e9bd356aa56a7f2cb2a49c
Jesse Shaw <JesseShawCodes@Gmail.com>
2024-05-10 21:04:37 -0400

    Backend Updates, Mock Data, Python requirements, CSV Generation in app.py

--- services/backend/app.py
+++ services/backend/app.py
@@ -1,9 +1,76 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, Response, send_file, jsonify
+import csv
 
-@app.route('/')
-def hello():
-	return "Hello World!"
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+# Sample data for demonstration
+users = [
+	{
+		"name": "Jesse Shaw",
+		"email": "Jdshaw1987@gmail.com"
+	}
+]
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+	if request.method == "POST":
+		name = request.form.get("name")
+		email = request.form.get("email")
+		users.append({"name": name, "email": email})
+	
+	return render_template("index.html", csv_data=users)
+
+@app.route("/test", methods=["GET"])
+def test_function():
+	return jsonify({"Test": "TEST"})
+
+@app.route("/generate_csv")
+def generate_csv():
+	if len(users) == 0:
+		return "No data to generate CSV."
+
+	# Create a CSV string from the user data
+	csv_data = "Name,Email\n"
+	for user in users:
+		csv_data += f"{user['name']},{user['email']}\n"
+
+	return render_template("index.html", csv_data=csv_data)
+
+@app.route("/download_csv")
+def download_csv():
+	if len(users) == 0:
+		return "No data to download."
+
+	# Create a CSV string from the user data
+	csv_data = "Name,Email\n"
+	for user in users:
+		csv_data += f"{user['name']},{user['email']}\n"
+
+	# Create a temporary CSV file and serve it for download
+	with open("users.csv", "w") as csv_file:
+		csv_file.write(csv_data)
+	print("Sending file...")
+	return send_file("users.csv", as_attachment=True, download_name="users.csv")
+
+@app.route("/download_csv_direct")
+def download_csv_direct():
+	if len(users) == 0:
+		return "No data to download."
+
+	# Create a CSV string from the user data
+	csv_data = "Name,Email\n"
+	for user in users:
+		csv_data += f"{user['name']},{user['email']}\n"
+
+	# Create a direct download response with the CSV data and appropriate headers
+	response = Response(csv_data, content_type="text/csv")
+	response.headers["Content-Disposition"] = "attachment; filename=users.csv"
+
+	return response
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
+	app.run(host='0.0.0.0', port=8000, debug=True)
---
+++ services/backend/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+python app.py run -h 0.0.0.0
